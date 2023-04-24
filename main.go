df767ffc528385834309a7830a4b9062589e4692
Abdulrahaman <olatunde.abdulrahaman@gmail.com>
2023-04-24 19:50:36 +0100

    persisting data

--- main.go
+++ main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
-	"reflect"
+	"os"
 	"strconv"
+
+	"database/sql"
+
+	"github.com/lib/pq"
+	"github.com/subosito/gotenv"
 
 	"github.com/gorilla/mux"
 )
@@ -18,15 +24,30 @@ type Book struct {
 }
 
 var books []Book
+var db *sql.DB
+
+func init() {
+	gotenv.Load()
+}
+
+func logFatal(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
-	router := mux.NewRouter()
 
-	books = append(books, Book{ID: 1, Title: "Golang Pointers", Author: "Mr. Golang", Year: "2010"},
-		Book{ID: 2, Title: "GoRoutines", Author: "Mr. GoRoutines", Year: "2011"},
-		Book{ID: 3, Title: "Golang Routers", Author: "Mr. Router", Year: "2012"},
-		Book{ID: 4, Title: "Golang Concurrency", Author: "Mr. Currency", Year: "2013"},
-		Book{ID: 5, Title: "Golang Good parts", Author: "Mr. Good", Year: "2014"})
+	pgUrl, err := pq.ParseURL(os.Getenv("ELEPHANTSQL_URL"))
+	logFatal(err)
+
+	db, err = sql.Open("postgres", pgUrl)
+	logFatal(err)
+
+	err = db.Ping()
+	logFatal(err)
+
+	router := mux.NewRouter()
 
 	router.HandleFunc("/books", getBooks).Methods("GET")
 	router.HandleFunc("/books/{id}", getBook).Methods("GET")
@@ -34,58 +55,82 @@ func main() {
 	router.HandleFunc("/books", updateBook).Methods("PUT")
 	router.HandleFunc("/books/{id}", removeBook).Methods("DELETE")
 
+	fmt.Println("Server is running on port 8000")
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
 
 func getBooks(w http.ResponseWriter, r *http.Request) {
 	// log.Println("Get all books is called")
+	var book Book
+	books = []Book{}
+	rows, err := db.Query("select * from books")
+	logFatal(err)
+
+	defer rows.Close()
+
+	for rows.Next() {
+		err := rows.Scan(&book.ID, &book.Title, &book.Author, &book.Year)
+		logFatal(err)
+
+		books = append(books, book)
+	}
+
 	json.NewEncoder(w).Encode(books)
 }
 
 func getBook(w http.ResponseWriter, r *http.Request) {
 	log.Println("Get book is called")
+
+	var book Book
+
 	params := mux.Vars(r)
 
-	i, _ := strconv.Atoi(params["id"])
+	rows := db.QueryRow("select * from books where id=$1", params["id"])
 
-	log.Println(reflect.TypeOf(i))
-	for _, book := range books {
-		if book.ID == i {
-			json.NewEncoder(w).Encode(&book)
-		}
-	}
+	err := rows.Scan(&book.ID, &book.Title, &book.Author, &book.Year)
+	logFatal(err)
+
+	json.NewEncoder(w).Encode(book)
 
 }
 func addBook(w http.ResponseWriter, r *http.Request) {
 	log.Println("Add book is called")
 	var book Book
-	log.Println(book)
+	var bookId int
 	json.NewDecoder(r.Body).Decode(&book)
+	err := db.QueryRow("insert into books (title, author, year) values($1, $2, $3) RETURNING id", &book.Title, &book.Author, &book.Year).Scan(&bookId)
+	logFatal(err)
 
-	books = append(books, book)
-	json.NewEncoder(w).Encode(books)
+	json.NewEncoder(w).Encode(bookId)
+
 }
 func updateBook(w http.ResponseWriter, r *http.Request) {
 	log.Println("Update Books is called")
 	var book Book
 	json.NewDecoder(r.Body).Decode(&book)
 
-	for i, item := range books {
-		if item.ID == book.ID {
-			books[i] = book
-		}
-	}
-	json.NewEncoder(w).Encode(books)
+	result, err := db.Exec("update books set title=$1, author=$2, year=$3 where id=$4 RETURNING id", &book.Title, &book.Author, &book.Year, &book.ID)
+	logFatal(err)
+
+	rowsUpdated, err := result.RowsAffected()
+	logFatal(err)
+
+	json.NewEncoder(w).Encode(rowsUpdated)
+
 }
 func removeBook(w http.ResponseWriter, r *http.Request) {
 	log.Println("Remove book is called")
+
 	params := mux.Vars(r)
 
-	id, _ := strconv.Atoi(params["id"])
-	for index, item := range books {
-		if item.ID == id {
-			books = append(books[:index], books[index+1:]...)
-		}
-	}
-	json.NewEncoder(w).Encode(books)
+	in, err := strconv.Atoi(params["id"])
+	logFatal(err)
+
+	result, err := db.Exec("delete from books where id=$1", in)
+	logFatal(err)
+
+	rowsDeleted, err := result.RowsAffected()
+	logFatal(err)
+
+	json.NewEncoder(w).Encode(rowsDeleted)
 }
